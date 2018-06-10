53c3a1c8807e865d7c39126c2a092995123ce954
yyeagy <39999910+yyeagy@users.noreply.github.com>
2018-06-10 14:50:46 -0400

    commit

--- buildScripts/scrServer.js
+++ buildScripts/scrServer.js
@@ -18,6 +18,15 @@ app.get('/', function(req,res){
   res.sendFile(path.join(__dirname,'../src/index.html'));
 });
 
+app.get('/users', function(req,res){
+  res.json([
+    {"id":1,"firsName":"Bob1","lastName":"Smith1","email":"bob1@email.com"},
+    {"id":2,"firsName":"Bob2","lastName":"Smith2","email":"bob2@email.com"},
+    {"id":3,"firsName":"Bob3","lastName":"Smith3","email":"bob3@email.com"},
+    {"id":4,"firsName":"Bob4","lastName":"Smith4","email":"bob4@email.com"}
+  ]);
+});
+
 app.listen(port, function(err){
   if(err){
     console.log(err);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import './index.css';
-import numeral from 'numeral'
+import {getUsers, deleteUser} from './api/userApi';
+ 
 
-/*eslint-disable  no-console*/
+getUsers().then( result =>{
+let usersBody = "";
+result.forEach(user => {
+    usersBody +=`<tr>
+    <td><a href="#" data-id="${user.id}" class="deleteUser"> Delete </a> </td>
+    <td>${user.id}</td>
+    <td>${user.firstName}</td>
+    <td>${user.lastName}</td>
+    <td>${user.email}</td>
+    </tr>`});
 
-const tValue = numeral(1000).format('$0,0.00');
-debugger;
-console.log('this is bundle test');
-console.log(`this is the ${tValue} for this class.`)
+global.document.getElementById('users').innerHTML=usersBody;
+
+const deleteLinks = global.document.getElementsByClassName('deleteUser');
+
+Array.from(deleteLinks, link => {
+    link.onclick = function(event){
+        const element = event.target;
+        event.preventDefault();
+        deleteUser(element.attributes["data-id"].value);
+        const row = element.parentNode.parentNode;
+        row.parentNode.removeChild(row);
+    };
+});
+});
